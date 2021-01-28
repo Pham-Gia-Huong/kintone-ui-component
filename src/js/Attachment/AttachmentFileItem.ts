4953de1c24b97f0088c4e488e7215b853d1b45dd
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2021-01-28 13:01:09 +0700

    bbb

--- src/js/Attachment/AttachmentFileItem.ts
+++ src/js/Attachment/AttachmentFileItem.ts
@@ -7,6 +7,7 @@ type AttachmentFileItemProps = ControlProps & {
   fileSize?: number;
   onFileRemove?: (index: number) => void;
 }
+console.log("b");
 
 class AttachmentFileItem extends Control<AttachmentFileItemProps> {
   protected element: HTMLDivElement
